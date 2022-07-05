4eb9fc298aaed471c4b609fcf13556bd9031343b
bala <balaceanu.bogdan@yahoo.com>
2022-07-05 13:27:34 -0700

    Add check for IP conflicts and display info tab
    Signed-off-by: bala <balaceanu.bogdan@yahoo.com>

--- app.py
+++ app.py
@@ -2,6 +2,8 @@ import ssl
 ssl._create_default_https_context = ssl._create_unverified_context
 from pyVmomi import vim
 from pyVim import connect
+from os import listdir
+from os.path import isfile, join
 import subprocess
 import sys
 import os
@@ -19,17 +21,48 @@ playbooks_map = {
     "wireshark": "install_wireshark.yml"
 }
 
-TOTAL_VMS_ALLOWED = 15
+PATH_TO_INFRASTRUCTURE_HISTORY_DIRECTORY = "/home/bala/Desktop/repo_licenta/licenta/infrastructures_created"
+
+TOTAL_VMS_ALLOWED = 20
 TOTAL_RAM_ALLOWED_ON_A_SINGLE_VM = 4096
 
 LINUX_ANSIBLE_HOSTS_FILE = "./linux/ansible/hosts"
 WINDOWS_ANSIBLE_HOSTS_FILE = "./windows/ansible/hosts"
 
+def checkExistenceOfInfrastructureDirectory():
+    isDir = os.path.isdir(PATH_TO_INFRASTRUCTURE_HISTORY_DIRECTORY) 
+    if isDir == False:
+        os.mkdir(PATH_TO_INFRASTRUCTURE_HISTORY_DIRECTORY)
+def checkNameOfInfrastructure(nameOfTfvarsFile):
+    tfvarsFiles = [f for f in listdir(PATH_TO_INFRASTRUCTURE_HISTORY_DIRECTORY) if isfile(join(PATH_TO_INFRASTRUCTURE_HISTORY_DIRECTORY, f))]
+    for tfvarsFile in tfvarsFiles:
+        if nameOfTfvarsFile == tfvarsFile:
+            return 1
+            
+    return 0
+
+def checkTfState(operating_system):
+    path = "/home/bala/Desktop/repo_licenta/licenta" + "/" + operating_system + "/terraform"
+    files = [f for f in listdir(path) if isfile(join(path, f))]
+    os.chdir(path)
+    for f in files:
+        if f == "terraform.tfstate" or f == "terraform.tfstate.backup":
+            os.remove(f)
+
 def generate_terraform_artefacts(config,tfvars_config):
+    
+    checkExistenceOfInfrastructureDirectory()
+    nameOfTfvarsFile = config['unique-id'] + ".tfvars"
+    if checkNameOfInfrastructure(nameOfTfvarsFile) == 1:
+        print("There is already an infrastructure with the name " + nameOfTfvarsFile + " in the infrastructure_created directory. Please modify the unique-id in config file.")
+        exit()
+    
+    destinationName = PATH_TO_INFRASTRUCTURE_HISTORY_DIRECTORY + "/" + config['unique-id'] + ".tfvars"
+    destinationNameForTfstate=PATH_TO_INFRASTRUCTURE_HISTORY_DIRECTORY + "/" + config['unique-id'] + ".tfstate"
     operating_system = config['vm-info']['operating-system']
     os.mkdir("/home/bala/Desktop/temp")  
     old_cwd = os.getcwd()  
-
+    
     if operating_system == "Linux":
         subprocess.call('cp /home/bala/Desktop/repo_licenta/licenta/linux/terraform/terraform.tfvars /home/bala/Desktop/temp', shell = True)
 
@@ -40,8 +73,13 @@ def generate_terraform_artefacts(config,tfvars_config):
             f_out.write(tfvars_file.format(**tfvars_config))
             
         os.chdir("./linux/terraform")
-        subprocess.call('terraform init && terraform plan -out plan&& terraform apply "plan"', shell = True)
-        shutil.copyfile('/home/bala/Desktop/repo_licenta/licenta/linux/terraform/terraform.tfvars','/home/bala/Desktop/repo_licenta/licenta/linux/terraform/terraformLastState.tfvars')
+        checkTfState("linux")
+
+        subprocess.call('terraform init && terraform plan -out plan && terraform apply "plan"', shell = True)
+        
+        shutil.move('/home/bala/Desktop/repo_licenta/licenta/linux/terraform/terraform.tfvars',destinationName)
+        shutil.move('/home/bala/Desktop/repo_licenta/licenta/linux/terraform/terraform.tfstate',destinationNameForTfstate)
+
         os.chdir(old_cwd)
 
         subprocess.call('cp /home/bala/Desktop/temp/terraform.tfvars /home/bala/Desktop/repo_licenta/licenta/linux/terraform/terraform.tfvars', shell = True)
@@ -57,8 +95,13 @@ def generate_terraform_artefacts(config,tfvars_config):
             f_out.write(tfvars_file.format(**tfvars_config))
         
         os.chdir("./windows/terraform")
-        subprocess.call('terraform init && terraform plan -out plan&& terraform apply "plan"', shell = True)
-        shutil.copyfile('/home/bala/Desktop/repo_licenta/licenta/windows/terraform/terraform.tfvars','/home/bala/Desktop/repo_licenta/licenta/windows/terraform/terraformLastState.tfvars')
+        checkTfState("windows")
+
+        subprocess.call('terraform init && terraform plan -out plan && terraform apply "plan"', shell = True)
+        
+        shutil.move('/home/bala/Desktop/repo_licenta/licenta/windows/terraform/terraform.tfvars',destinationName)
+        shutil.move('/home/bala/Desktop/repo_licenta/licenta/windows/terraform/terraform.tfstate',destinationNameForTfstate)
+
         os.chdir(old_cwd)
         
         subprocess.call('cp /home/bala/Desktop/temp/terraform.tfvars /home/bala/Desktop/repo_licenta/licenta/windows/terraform/terraform.tfvars', shell = True)
@@ -85,9 +128,33 @@ def install_toolset(config):
 
     print("Tool installation is complete")
 
+def getListOfIpInVCenter(listofIpInVCenter,config):
+    si = connect.SmartConnect(host=config['vsphere-info']['vsphere-vcenter'], user=config['vsphere-info']['vsphere-user'], pwd=config['vsphere-info']['vsphere-password'], port=443)
+    content = si.RetrieveContent()
+    for vm in get_vim_objects(content, vim.VirtualMachine):
+        if vm.config: 
+            listofIpInVCenter.append(vm.guest.ipAddress) 
+    return listofIpInVCenter
+
+def checkIpRange(config):
+    ipStart = config['network-info']['vm-ipv4-root'] + config['network-info']['vm-ipv4-start']
+    count = config['vm-info']['vm-count']
+    listofIpInVCenter = []
+    listofIpInVCenter = getListOfIpInVCenter(listofIpInVCenter,config)
+    listofIpInConfigFile = []
+    for i in range(int(count)):
+        lastNumb = int(config['network-info']['vm-ipv4-start']) + int(i)   
+        ip = config['network-info']['vm-ipv4-root'] + str(lastNumb)
+        listofIpInConfigFile.append(ip)
+    
+    for ipInConfigFile in listofIpInConfigFile:
+        for ipInVCenter in listofIpInVCenter:
+            if ipInConfigFile == ipInVCenter:
+                print(ipInConfigFile + " is already used in vCenter, please reconfigure the network settings in config file.")
+                exit()
+        
 
 def checkResources(args):
-
     totalVmCountInFile = 0
     for arg in args.config:        
         with open(arg, 'r') as f_config:
@@ -99,41 +166,64 @@ def checkResources(args):
                 print("You entered too much RAM memory. Exiting...")
                 exit()
             totalVmCountInFile += int(config['vm-info']['vm-count'])
+            checkIpRange(config)
     if totalVmCountInFile > TOTAL_VMS_ALLOWED:
         print("You entered too many vm's. There is no enough space. Exiting...")
         exit()
     
 
 def deleteInfrastrcture():
-    if sys.argv[2] != "Linux" and sys.argv[2] != "Windows":
-        print("You can only use \"Linux\" or \"Windows\"")
+    infrastructureName = sys.argv[2] + ".tfvars"
+    infratructureNameStatev = sys.argv[2] + ".tfstate"
+    if checkNameOfInfrastructure(infrastructureName) == 0 or checkNameOfInfrastructure(infratructureNameStatev) == 0:
+        print("There is no file called " + infrastructureName + " or " + infratructureNameStatev + " in the infrastructure_created directory.")
         exit()
-    old_cwd = os.getcwd()
-    if sys.argv[2] == "Linux":
-        os.chdir("./linux/terraform")  
-        shutil.copyfile('/home/bala/Desktop/repo_licenta/licenta/linux/terraform/terraform.tfvars','/home/bala/Desktop/terraform.tfvars')
+
+    #ii fac load
+    pathForTfvars = PATH_TO_INFRASTRUCTURE_HISTORY_DIRECTORY + "/" + infrastructureName
+    pathForTfstate = PATH_TO_INFRASTRUCTURE_HISTORY_DIRECTORY + "/" + infratructureNameStatev
+    #ii iau sistemul de operare
+    #os.chdir(PATH_TO_INFRASTRUCTURE_HISTORY_DIRECTORY)
+    with open(pathForTfstate, 'r') as f_config:
+        config = json.load(f_config)
+    if config['resources'][4]['instances'][0]['attributes']['name'] == "Ubuntu-1604-Template":
+        operating_system = "Linux"
+    else:
+        if config['resources'][4]['instances'][0]['attributes']['name'] == "Win2016-Template":
+            operating_system = "Windows"
+        else:
+            print("The name of template in vCenter are different from the existing one tfstate file. The destruction cannot be completed...")
+            exit()
+
+    #ii iau sistemul de operare
+    if operating_system == "Linux":
+        # iau sablonul cu cut pe desktop
+        shutil.move('/home/bala/Desktop/repo_licenta/licenta/linux/terraform/terraform.tfvars','/home/bala/Desktop/terraform.tfvars')
+        # iau fisierele si le duc in terraform
+        shutil.move(pathForTfvars,'/home/bala/Desktop/repo_licenta/licenta/linux/terraform/terraform.tfvars')
+        shutil.move(pathForTfstate,'/home/bala/Desktop/repo_licenta/licenta/linux/terraform/terraform.tfstate')
+        os.chdir('/home/bala/Desktop/repo_licenta/licenta/linux/terraform')
+        subprocess.call('terraform destroy --auto-approve', shell = True)
+        #sterg ala folosit din terrafrom
         os.remove('/home/bala/Desktop/repo_licenta/licenta/linux/terraform/terraform.tfvars')
-        os.rename('/home/bala/Desktop/repo_licenta/licenta/linux/terraform/terraformLastState.tfvars','/home/bala/Desktop/repo_licenta/licenta/linux/terraform/terraform.tfvars')
+        os.remove('/home/bala/Desktop/repo_licenta/licenta/linux/terraform/terraform.tfstate')
+        #duc inapoi sablonul de pe desktop
+        shutil.move('/home/bala/Desktop/terraform.tfvars','/home/bala/Desktop/repo_licenta/licenta/linux/terraform/terraform.tfvars')
 
+    if operating_system == "Windows":
+        # iau sablonul cu cut pe desktop
+        shutil.move('/home/bala/Desktop/repo_licenta/licenta/windows/terraform/terraform.tfvars','/home/bala/Desktop/terraform.tfvars')
+        # iau fisierele si le duc in terraform
+        shutil.move(pathForTfvars,'/home/bala/Desktop/repo_licenta/licenta/windows/terraform/terraform.tfvars')
+        shutil.move(pathForTfstate,'/home/bala/Desktop/repo_licenta/licenta/windows/terraform/terraform.tfstate')
+        os.chdir('/home/bala/Desktop/repo_licenta/licenta/windows/terraform')
         subprocess.call('terraform destroy --auto-approve', shell = True)
-        
-        os.rename('/home/bala/Desktop/repo_licenta/licenta/linux/terraform/terraform.tfvars','/home/bala/Desktop/repo_licenta/licenta/linux/terraform/terraformLastState.tfvars')
-        shutil.copyfile('/home/bala/Desktop/terraform.tfvars','/home/bala/Desktop/repo_licenta/licenta/linux/terraform/terraform.tfvars')
-        os.remove('/home/bala/Desktop/terraform.tfvars')
-        
-    if sys.argv[2] == "Windows":
-        os.chdir("./windows/terraform")   
-        shutil.copyfile('/home/bala/Desktop/repo_licenta/licenta/windows/terraform/terraform.tfvars','/home/bala/Desktop/terraform.tfvars')
+        #sterg ala folosit din terrafrom
         os.remove('/home/bala/Desktop/repo_licenta/licenta/windows/terraform/terraform.tfvars')
-        os.rename('/home/bala/Desktop/repo_licenta/licenta/windows/terraform/terraformLastState.tfvars','/home/bala/Desktop/repo_licenta/licenta/windows/terraform/terraform.tfvars')
+        os.remove('/home/bala/Desktop/repo_licenta/licenta/windows/terraform/terraform.tfstate')
+        #duc inapoi sablonul de pe desktop
+        shutil.move('/home/bala/Desktop/terraform.tfvars','/home/bala/Desktop/repo_licenta/licenta/windows/terraform/terraform.tfvars')
 
-        subprocess.call('terraform destroy --auto-approve', shell = True)
-        
-        os.rename('/home/bala/Desktop/repo_licenta/licenta/windows/terraform/terraform.tfvars','/home/bala/Desktop/repo_licenta/licenta/windows/terraform/terraformLastState.tfvars')
-        shutil.copyfile('/home/bala/Desktop/terraform.tfvars','/home/bala/Desktop/repo_licenta/licenta/windows/terraform/terraform.tfvars')
-        os.remove('/home/bala/Desktop/terraform.tfvars')  
-    
-    os.chdir(old_cwd)
 
 def get_vim_objects(content, vim_type):
     '''Get vim objects of a given type.'''
@@ -143,7 +233,7 @@ def getTemplatesFromVCenter(listOfTemplates,config):
     si = connect.SmartConnect(host=config['vsphere-info']['vsphere-vcenter'], user=config['vsphere-info']['vsphere-user'], pwd=config['vsphere-info']['vsphere-password'], port=443)
     content = si.RetrieveContent()
     for vm in get_vim_objects(content, vim.VirtualMachine):
-        if vm.config.template: 
+        if vm.config.template:         
             listOfTemplates.append(vm.name)
     return listOfTemplates
 
@@ -158,8 +248,12 @@ def checkTemplate(config,arg):
     else:
         print("You have entered a template which does not exist in vCenter.")
         print("Do you want to deploy an infrastructure using an existing template? 1 - yes or 2 - no")
-        choice = int(input())
-        if choice == 1:
+        print("You entered more than 1 config file. You must be aware of network mistakes. Do you want to continue? 1 - Yes or 2 - NO")
+        choice = input()
+        if choice.isdigit() != 1:
+            print("You entered something different a number...exiting")
+            exit()
+        if int(choice) == 1:
             if len(listOfTemplates) == 0:
                 print("There are no templates existing in vCenter")
                 print("Contact the administrator to build some templates...")
@@ -173,7 +267,7 @@ def checkTemplate(config,arg):
                 print("You entered a wrong number...exiting")
                 exit()
             templateChoiceName = listOfTemplates[templateChoiceNumber - 1]
-            #schimba in fisier            
+                        
             with open(arg, "r") as jsonFile:
                 data = json.load(jsonFile)            
 
@@ -188,7 +282,6 @@ def checkTemplate(config,arg):
             
             print("The deployment will start soon...")
             return 1
-          
 
         else:
             print("Exiting...")
@@ -198,7 +291,7 @@ def checkTemplate(config,arg):
 def generate_ansible_host_file(config):
     if len(config['vm-info']['toolset']) == 0:
         print("No tools need to be installed")
-        exit()
+        return()
 
     operating_system = config['vm-info']['operating-system']
 
@@ -230,35 +323,62 @@ def generate_ansible_host_file(config):
 
         with open('./windows/ansible/hosts'.format(config), 'w+') as f_out:
             f_out.write(hosts.format(**info))
+    install_toolset(config)
 
-if __name__ == '__main__':
+def getInformationAboutVMs(config):
+    si = connect.SmartConnect(host=config['vsphere-info']['vsphere-vcenter'], user=config['vsphere-info']['vsphere-user'], pwd=config['vsphere-info']['vsphere-password'], port=443)
+    content = si.RetrieveContent()
+    for vm in get_vim_objects(content, vim.VirtualMachine):
+        if vm.config:
+            if str(vm.guest.ipAddress) == "None":
+                print(("Could not get the IP address of VM with name ") + str(vm.name) + " because is shutted down. Power on the VM to get its IP.")
+            else:
+                print(("VM with name ") + str(vm.name) + (" has the following IP address: ") + str(vm.guest.ipAddress))
+
+
+if __name__=='__main__':
     parser = ArgumentParser(description = 'This module is used to automatically deploy a Windows/Linux infrastructure')
     parser.add_argument('-c', '--config', nargs='+', help = 'Path to configuration file', required = False)
     parser.add_argument('-d', '--delete', nargs='+', help = 'Linux or Windows infrastructure to be deleted', required = False)
+    parser.add_argument('-i', '--info', nargs='+', help = 'Information about existing virtual machines in vCenter', required = False)
     args = parser.parse_args()
 
     if not len(sys.argv) > 1:
         print("The script must be called with arguments")
         print("Use -c <config_file.json> for deploying an infrastructure")
-        print("Use -d <Linux/Windows> for deleting the last infrastructure created with Linux or Windows")
+        print("Use -d <unique-id> for deleting the infrastructure with that id")
+        print("Use -i <config_file.json> for obtaining information about existing virtual machines in vCenter")
         exit()
-    
+
     if sys.argv[1] == "-d" or sys.argv[1] == "--delete":
         deleteInfrastrcture()
         exit()
-  
-    checkResources(args)
 
-    for arg in args.config:        
+    if sys.argv[1] == "-i" or sys.argv[1] == "--info":
+        with open(sys.argv[2], 'r') as f_config:
+            config = json.load(f_config)
+        getInformationAboutVMs(config)
+        exit()
+    
+    
+
+    if len(sys.argv) - 2 >= 2:
+        print("You entered more than 1 config file. You must be aware of network mistakes. Do you want to continue? 1 - Yes or 2 - NO")
+        choice = input()
+        if choice.isdigit() != 1:
+            print("You entered something different from 1 or 2...exiting")
+            exit()
+        if int(choice) == 2:
+            exit()
+
+    checkResources(args)    
+
+    for arg in args.config:   
         with open(arg, 'r') as f_config:
             config = json.load(f_config)
-        changed = checkTemplate(config,arg)    
-        if changed == 1:  
-            with open(arg, 'r') as f_config:
-                config = json.load(f_config)        
+        checkTemplate(config,arg)     
         generate_terraform_artefacts(config,{**config['vsphere-info'], **config['network-info'], **config['vm-info']})
         generate_ansible_host_file(config)
-        install_toolset(config)
+        
 
     print("Done..")
-    
